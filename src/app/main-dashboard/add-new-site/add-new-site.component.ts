f4f621079438084c79c7f5a541cdffe30567c2b3
Hari Chandana <harichandanad12@gmail.com>
2022-06-24 17:25:17 +0530

    site Component with accordion

--- src/app/main-dashboard/add-new-site/add-new-site.component.ts
+++ src/app/main-dashboard/add-new-site/add-new-site.component.ts
@@ -7,11 +7,11 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class AddNewSiteComponent implements OnInit {
 
-  @Input() show:any;
+  @Input() show: any;
 
   @Output() newItemEvent = new EventEmitter<boolean>();
-  
-  closeAddSite(value:boolean) {
+
+  closeAddSite(value: boolean) {
     this.newItemEvent.emit(value);
   }
 
@@ -21,38 +21,66 @@ export class AddNewSiteComponent implements OnInit {
     console.log(this.show)
   }
 
-  isShown: boolean = false ; // hidden by default
-  
+  isShown: boolean = false; // hidden by default
+
   toggleShowOnOff() {
-    this.isShown = ! this.isShown;
+    this.isShown = !this.isShown;
   }
 
   Monitoring: boolean = false;
 
   toggleShowMonit() {
-    this.Monitoring = ! this.Monitoring;
+    this.Monitoring = !this.Monitoring;
   }
 
   Business: boolean = false;
 
   toggleShowBusiness() {
-    this.Business = ! this.Business;
+    this.Business = !this.Business;
   }
 
   existSecuity: boolean = false;
 
-  toggleShowExistSecuity(value:any, type:any) {
-    if(type=='security'){
-      if(value=='on'){this.existSecuity = true;}
-      else{this.existSecuity = false;}
+  toggleShowExistSecuity(value: any, type: any) {
+    if (type == 'security') {
+      if (value == 'on') {
+        this.existSecuity = true;
+      }
+      else {
+        this.existSecuity = false;
+      }
     }
+  }
 
+  internet: boolean = false;
+
+  toggleShowInternet(value: any, type: any) {
+    if (type == 'internet') {
+      if (value == 'on') {
+        this.internet = true;
+      }
+      else {
+        this.internet = false;
+      }
+    }
+  }
+
+  moni: boolean = false;
+
+  monitoring() {
+    this.moni = !this.moni;
+  }
+ 
+  intell: boolean = false;
+  
+  businessIntell() {
+    this.intell = !this.intell;
   }
 
   // checkbox(e:any, type:any){
-    // if(type== 'preinst'){
-    //   console.log(e.target.checked, type)
-    // }
+  // if(type== 'preinst'){
+  //   console.log(e.target.checked, type)
+  // }
   //   if (document.querySelector('#bopis:checked')) {
   //     console.log(e.target.checked, type);
   //   }
