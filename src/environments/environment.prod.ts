52f5ce22916ab65f24da957956ded0d7d70ad706
Veer <97098333+itsVeer007@users.noreply.github.com>
2024-10-24 12:03:38 +0530

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  // baseUrl: 'http://usmgmt.iviscloud.net:777',
   adsAndInventoryUrl: 'http://usstaging.ivisecurity.com:8080',
   authUrl: 'http://34.206.37.237:80',
-  siteUrl: 'http://rsmgmt.ivisecurity.com:8943',
+  baseUrl: 'http://rsmgmt.ivisecurity.com:8943',
   emailUrl: 'http://usstaging.ivisecurity.com:8943',
 };
